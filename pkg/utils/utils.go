40d3cfac0b7269eb5054468d09795a923e30280f
pmf <faizpraditya7@gmail.com>
2024-01-20 08:41:30 +0700

    unmarshal the data in utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// we need unmarshal data, since it will be in json (data from user)
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
