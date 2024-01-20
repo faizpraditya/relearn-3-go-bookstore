1d9f5fdcab0173f3c59f069bcf55047e4e11cae2
pmf <faizpraditya7@gmail.com>
2024-01-20 08:31:35 +0700

    connect to postgres db

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"go-bookstore/pkg/config" // Replace with the actual path to your config package
+)
+
+func main() {
+	config.Connect()
+	defer config.Disconnect() // Ensure that the connection is closed when main function exits
+
+	// Check if the database connection is successful
+	if err := config.GetDB().DB().Ping(); err != nil {
+		fmt.Println("Failed to connect to the database:", err)
+	} else {
+		fmt.Println("Successfully connected to the database.")
+	}
+}
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	// help the others file to interact with the DB
+	db *gorm.DB
+)
+
+func Connect() {
+	// username := os.Getenv("db_user")
+	username := "postgres"
+	password := "admin"
+	dbName := "practice"
+	dbHost := "localhost"
+	// add port because i have 2 version postgres, and the one that i use is not on the default port
+	dbPort := "5433"
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", dbHost, username, dbName, password, dbPort)
+
+	// if there's an error, the error will come into err, if not it will come into d
+	d, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func Disconnect() {
+	if db != nil {
+		db.Close()
+	}
+}
