b6ec19aa455f69db58ba450e298321f9d385a176
pmf <faizpraditya7@gmail.com>
2024-01-21 08:11:22 +0700

    book models and init func

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,23 @@
 package models
+
+import (
+	"go-bookstore/pkg/config"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+// helper to intialize with the db in the first place
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
